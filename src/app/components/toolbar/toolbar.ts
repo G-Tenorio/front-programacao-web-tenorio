ac624b789336cb67eadb5b27d714a2c72eaeb768
G-Tenorio <gustavobertazotenorio@gmail.com>
2025-08-25 22:25:35 -0300

    Implementação Toolbar e correção de tema do Primeng

---
+++ src/app/components/toolbar/toolbar.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { ToolbarModule } from 'primeng/toolbar';
+
+@Component({
+  selector: 'app-toolbar',
+  imports: [ButtonModule, ToolbarModule ],
+  templateUrl: './toolbar.html',
+  styleUrl: './toolbar.scss'
+})
+export class Toolbar {
+
+}
